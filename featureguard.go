f99daa869f7463acd8ee69299b54116d291a6871
Ade Syahlan Prayoga <ade.prayoga@erajaya.com>
2023-12-11 15:36:40 +0700

    Implements basic functions and initializes test

--- featureguard.go
+++ featureguard.go
@@ -2,48 +2,77 @@ package featureguard
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/redis/go-redis/v9"
 )
 
-var (
-	defaultRedisKeyPrefix = "featureguard:"
-)
+const defaultKeyPattern = "featureguard:%s"
 
 // FeatureGuard represents a feature flags management system using Redis as the storage backend.
 type FeatureGuard struct {
-	client    *redis.Client
-	keyPrefix string
+	db         *redis.Client
+	keyPattern string
 }
 
-// NewFeatureGuard initializes a new FeatureGuard instance with the provided Redis client connection and key prefix.
-func NewFeatureGuard(redisClient *redis.Client, redisKeyPrefix string) *FeatureGuard {
-	if redisKeyPrefix == "" {
-		redisKeyPrefix = defaultRedisKeyPrefix
+// NewFeatureGuard initializes a new FeatureGuard instance with the provided Redis client connection.
+// If keyPattern is empty, it uses the default key pattern.
+func NewFeatureGuard(db *redis.Client, keyPattern string) *FeatureGuard {
+	if keyPattern == "" {
+		keyPattern = defaultKeyPattern
 	}
 
 	return &FeatureGuard{
-		client:    redisClient,
-		keyPrefix: redisKeyPrefix,
+		db:         db,
+		keyPattern: keyPattern,
 	}
+}
+
+// getKey formats the feature key using the specified pattern.
+func (fg *FeatureGuard) getKey(feature string) string {
+	return fmt.Sprintf(fg.keyPattern, feature)
 }
 
 // EnableFeature enables the specified feature flag.
 func (fg *FeatureGuard) EnableFeature(ctx context.Context, feature string) error {
-	return nil
+	key := fg.getKey(feature)
+	return fg.db.Set(ctx, key, true, 0).Err()
 }
 
 // DisableFeature disables the specified feature flag.
 func (fg *FeatureGuard) DisableFeature(ctx context.Context, feature string) error {
-	return nil
+	key := fg.getKey(feature)
+	return fg.db.Del(ctx, key).Err()
+}
+
+// IsFeatureEnabled checks if the specified feature flag is enabled.
+func (fg *FeatureGuard) IsFeatureEnabled(ctx context.Context, feature string) (bool, error) {
+	key := fg.getKey(feature)
+
+	result, err := fg.db.Get(ctx, key).Bool()
+	if err != nil {
+		if err == redis.Nil {
+			// Key does not exist, feature is considered disabled
+			return false, nil
+		}
+
+		// Other error occurred
+		return false, err
+	}
+
+	return result, nil
 }
 
 // ToggleFeature toggles the state of the specified feature flag.
 func (fg *FeatureGuard) ToggleFeature(ctx context.Context, feature string) error {
-	return nil
-}
+	key := fg.getKey(feature)
 
-// IsFeatureEnabled checks if the specified feature flag is enabled
-func (fg *FeatureGuard) IsFeatureEnabled(ctx context.Context, feature string) (bool, error) {
-	return false, nil
+	// Check the current state
+	currentState, err := fg.IsFeatureEnabled(ctx, feature)
+	if err != nil {
+		return err
+	}
+
+	// Toggle the state
+	return fg.db.Set(ctx, key, !currentState, 0).Err()
 }
--- featureguard_test.go
+++ featureguard_test.go
@@ -2,56 +2,8 @@ package featureguard
 
 import (
 	"testing"
-
-	"github.com/go-redis/redismock/v9"
-	"github.com/redis/go-redis/v9"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestNewFeatureGuard(t *testing.T) {
-	redisClientMock, _ := redismock.NewClientMock()
 
-	type args struct {
-		redisClient    *redis.Client
-		redisKeyPrefix string
-	}
-	tests := []struct {
-		name string
-		args args
-		want *FeatureGuard
-	}{
-		{
-			name: "Given initialization a new FeatureGuard instance " +
-				"with the Redis client connection and no key prefix provided " +
-				"Then it should return FeatureGuard instance with default key prefix",
-			args: args{
-				redisClient:    redisClientMock,
-				redisKeyPrefix: "",
-			},
-			want: &FeatureGuard{
-				client:    redisClientMock,
-				keyPrefix: defaultRedisKeyPrefix,
-			},
-		},
-		{
-			name: "Given initialization a new FeatureGuard instance " +
-				"with the Redis client connection and key prefix provided " +
-				"Then it should return FeatureGuard instance with provided key prefix",
-			args: args{
-				redisClient:    redisClientMock,
-				redisKeyPrefix: "prefix",
-			},
-			want: &FeatureGuard{
-				client:    redisClientMock,
-				keyPrefix: "prefix",
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := NewFeatureGuard(tt.args.redisClient, tt.args.redisKeyPrefix)
-
-			assert.Equal(t, tt.want, got)
-		})
-	}
 }
